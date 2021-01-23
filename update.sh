a25cc663aea0bcf912f84355f0a2345b43e09ce1
George Ioakimedes <georgeioak@comcast.net>
2021-01-22 16:55:09 -0800

    minor corrections

--- update.sh
+++ update.sh
@@ -6,26 +6,27 @@ reset="\e[0m"
 
 
 set +e #
-echo -e "\e[92mStopping Service and Grabbing New Code...\e[0m" #
+echo -e "$green Stopping Service and Grabbing New Code...$reset" #
 echo "" #
 echo "_________________________________________________________ " #
 sudo systemctl stop oden.service
 cd /home/volumio/Oden
 #git reset --hard
 git config core.filemode false  # Copying from Windows changes execute bit on Linux so ignore it
+# Can run git add --chmod=+x update.sh on Windows to set execute bit
 git pull
 
 thefile='/usr/lib/libbcm_host.so'
 if [[ -L $thefile ]]; then
     if [[ -e $thefile ]]; then
-        echo -e "${green}it's already there, no worries...${reset}"
+        echo -e "$green The file is already there, no worries...$reset"
     else
         unlink $thefile
-        echo -e "${boldRed}$thefile link was broken and removed"
-        echo -e "Let George know there's a problem!!${reset}"
+        echo -e "$boldRed $thefile link was broken and removed"
+        echo -e "Let George know there's a problem!!$reset"
     fi
 else
-    echo -e "${yellow}$thefile isn't a link, I'll add it${reset}"
+    echo -e "$yellow$thefile isn't a link, I'll add it$reset"
     sudo systemctl stop mpd.service
     sudo ln -s /opt/vc/lib/libbcm_host.so /usr/lib/
     sudo systemctl start mpd.service
@@ -34,4 +35,4 @@ fi
 sudo systemctl start oden.service
 echo "" #
 echo "_________________________________________________________ " #
-echo -e "\e[92mCode has been updated...\e[0m" #
+echo -e "$green Code has been updated...$reset" #
